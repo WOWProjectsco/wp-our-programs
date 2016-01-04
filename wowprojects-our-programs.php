7f9a379c30eab3f98d5c80aca6c9af6edbe2cdb9
XicoOfficial <xicoofficial@gmail.com>
2016-01-03 20:46:07 -0600

    Add custom taxonomies

--- wowprojects-our-programs.php
+++ wowprojects-our-programs.php
@@ -3,7 +3,7 @@
 /**
 * Plugin Name: Our Programs
 * Plugin URI: http://wowdevshop.com
-* Description: This plugin registers the 'product' post type, it let's you manage your companies programs.
+* Description: This plugin registers the 'program' post type, it let's you manage your companies programs.
 * Author: WowDevHop
 * Version: 1.0.1
 * License: GPLv2
@@ -42,13 +42,50 @@ function wpmudev_create_post_type() {
 		'has_archive' => true,
  		'public' => true,
 		'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
-		'taxonomies' => array('category' ),
+		'taxonomies' => array('program-category' ),
 		'exclude_from_search' => false,
 		'capability_type' => 'post',
 		'rewrite' => array( 'slug' => 'programs' ),
         'menu_icon' => 'dashicons-format-aside',
+        'menu_position' => 6
 		)
 	);
+}
+
+
+
+// hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', 'create_program_taxonomies', 0 );
+
+
+// Create own taxonomies for the opst type "program"
+function create_program_taxonomies() {
+    //Add new taxonomi, make it hierarchical (like categories)
+    $labels = array(
+        'name'              => _x( 'Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Categories' ),
+        'all_items'         => __( 'All Categories' ),
+        'parent_item'       => __( 'Parent Category' ),
+        'parent_item_colon' => __( 'Parent Category:' ),
+        'edit_item'         => __( 'Edit Category' ),
+        'update_item'       => __( 'Update Category' ),
+        'add_new_item'      => __( 'Add New Category' ),
+        'new_item_name'     => __( 'New Category Name' ),
+        'menu_name'         => __( 'Category' ),
+    );
+
+
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'program-category' ),
+    );
+
+    register_taxonomy( 'program-category', array( 'program' ), $args );
 }
 
 ?>
